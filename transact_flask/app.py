39ea916097d82626fa6f067af15c5dc38764a634
Kent Morris <kentmorris@sandiego.edu>
2024-11-12 18:19:16 -0800

    Added UI Design into Flask Server

--- transact_flask/app.py
+++ transact_flask/app.py
@@ -1,36 +1,25 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
 
+def create_app(test_config=None):
+    
+    app = Flask(__name__)
+    
+    @app.route("/home")
+    def home_screen():
+        return render_template("main.html")
 
-@app.route("/home")
-def home_screen():
-    return render_template("index.html")
+    @app.route("/comment")
+    def logout():
+        return render_template("comment.html")
 
+    @app.route("/login")
+    def login():
+        return render_template("login.html")
 
-@app.route("/register", endpoint="register")
-def signup_screen():
-    return render_template("auth/register.html")
+    app.run(host="0.0.0.0", port=80)
 
+    return app
 
-@app.route("/login")
-def login():
-    return render_template("auth/login.html")
-
-
-@app.route("/logout")
-def logout():
-    return "Log Out!"
-
-
-@app.route("/posting")
-def posting_screen():
-    return render_template("base.html")
-
-
-@app.route("/commenting")
-def commenting_screen():
-    return render_template("base.html")
-
-
-app.run(host="0.0.0.0", port=80)
+var = create_app()
+var.run()
