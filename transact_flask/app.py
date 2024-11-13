3573d61511f9636c06933ba5153df2d57b0a8dc4
Nate R <naterivera@sandiego.edu>
2024-11-12 17:15:26 -0800

    Created the database connection and basic inserts

--- transact_flask/app.py
+++ transact_flask/app.py
@@ -1,7 +1,25 @@
 from flask import Flask, render_template
+import psycopg2
 
 app = Flask(__name__)
 
+# Connect to the database 
+conn = psycopg2.connect(database="intro_project", user="postgres", password="transact", host="localhost", port="5432") 
+
+# create a cursor 
+cur = conn.cursor() 
+
+# Insert some data into the table 
+cur.execute( 
+    '''INSERT INTO User (email, name, admin) VALUES \ 
+    ('Jimmy@sandiego.edu', 'Jimmy',  TRUE), ('Humpfre@sandiego.edu', 'Humpfre', FALSE), ('Diego@sandiego.edu', 'Diego', FALSE);''') 
+cur.execute( 
+    '''INSERT INTO Post (contents, user_id, created_at) VALUES \ 
+    ('What's your favorite color?', 1, '2024-11-13 01:01:01'), ('Anyone finish their project already?', 2, '2024-11-14 23:59:59');''') 
+cur.execute( 
+    '''INSERT INTO Comment (contents,, user_id, post_id, created_at) VALUES \ 
+    ('Apple', 2, 1, '2024-11-13 01:02:03'), ('Blue', 3, 1, '2024-11-13 02:02:03'), 
+    ('Nope', 3, 2, '2024-11-15 03:15:00'), ('RIP', 1, 2, '2024-11-15 09:30:40');''') 
 
 @app.route("/home")
 def home_screen():
