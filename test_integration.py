45de33e0b75f0ada425e462789aff8297db4df05
MathishaKaru <116924133+MathishaKaru@users.noreply.github.com>
2024-11-13 20:04:07 -0800

    Update test_integration.py

--- test_integration.py
+++ test_integration.py
@@ -33,11 +33,3 @@ def app():
 @pytest.fixture
 def client(app):
     return app.test_client()
-
-# Test function to check if the homepage loads successfully
-def test_homepage_loads(client):
-    # Send a GET request 
-    response = client.get('/public')
-    
-    # Assert that the response status code is 200 (OK)
-    assert response.status_code == 200
